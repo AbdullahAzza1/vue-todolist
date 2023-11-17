509d4cf20b79067b3ae4dae308ae3cd435baa317
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-17 16:55:38 +0100

    step1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-const {creteApp} = Vue;
+const { creteApp } = Vue;
 
 const opzioni = {
     data: function () {
         return {
-            
+            lista: [
+                {
+                    domanda1: 'hai fatto i compito?'
+                },
+                {
+                    domanda2: 'hai fatto la spesa?'
+                },
+                {
+                    domanda3: 'hai portato fuori il cane?'
+                }
+            ]
         }
     }
 }
+
+createApp(opzioni).mount('#app')
